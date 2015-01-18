ab2e1b651b033f530ab6d2460bb36ea786312876
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2015-01-18 01:22:54 +0100

    cleaning podcast description

--- src/main/java/de/nosebrain/podcastrename/controller/BaseController.java
+++ src/main/java/de/nosebrain/podcastrename/controller/BaseController.java
@@ -2,6 +2,7 @@ package de.nosebrain.podcastrename.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.rometools.rome.feed.synd.SyndContent;
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
@@ -17,6 +20,8 @@ import com.rometools.rome.io.XmlReader;
 
 @Controller
 public class BaseController {
+  
+  private static final Pattern DESCRIPTION_PATTERN = Pattern.compile("<div><span style=\"color:#666666;font-size:11px;\">Views:</span>.*", Pattern.DOTALL);
 
   @RequestMapping("/")
   public void renamePodcast(@RequestParam("url") final URL url, @RequestParam("title") final String title, final HttpServletResponse response) throws IOException, IllegalArgumentException, FeedException {
@@ -24,6 +29,13 @@ public class BaseController {
     final SyndFeedInput input = new SyndFeedInput();
     final SyndFeed feed = input.build(new XmlReader(url));
     feed.setTitle(title);
+    
+    for (final SyndEntry entry : feed.getEntries()) {
+      final SyndContent description = entry.getDescription();
+      final String cleanedDescription = DESCRIPTION_PATTERN.matcher(description.getValue()).replaceAll("");
+      description.setValue(cleanedDescription);
+      System.err.println(description.getValue());
+    }
     
     final SyndFeedOutput output = new SyndFeedOutput();
     output.output(feed, response.getWriter());
